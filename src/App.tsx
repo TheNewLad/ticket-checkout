c0b47ea6ddedb8dd341b2b6a495d8469ba85461d
devkdn <22102599+devkdn@users.noreply.github.com>
2023-04-15 11:34:33 -0500

    Rename setSelectedShow to onShowSelect for consistency

--- src/App.tsx
+++ src/App.tsx
@@ -34,7 +34,7 @@ export default function App() {
         return (
           <ShowSelector
             selectedShow={selectedShowId}
-            setSelectedShow={setSelectedShowId}
+            onShowSelect={setSelectedShowId}
           />
         );
       case 1:
