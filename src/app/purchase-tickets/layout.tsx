a23415b6f2511e1c2d06a33098ef7fa545d93773
devkdn <22102599+devkdn@users.noreply.github.com>
2023-06-19 14:30:13 -0500

    Checkout layout displays the current step

---
+++ src/app/purchase-tickets/layout.tsx
@@ -0,0 +1,50 @@
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <div>
+      <h1>Purchase Tickets</h1>
+      <div className={"flex"}>
+        <Step step={1} active={true} text={"Select A Show"} />
+        <Step step={2} active={false} text={"Select Ticket Quantity"} />
+        <Step step={3} active={false} text={"Checkout"} />
+      </div>
+      {children}
+    </div>
+  );
+}
+
+// TODO: Move this to component directory
+interface StepProps {
+  step: number;
+  active: boolean;
+  text: string;
+}
+const Step = ({ step, active, text }: StepProps) => {
+  return (
+    <div className={"mx-2 flex items-center"}>
+      <Label step={step} active={active} />
+      <p className={`${active ? "" : "hidden"}`}>{text}</p>
+    </div>
+  );
+};
+
+const Label = ({ step, active }: Omit<StepProps, "text">) => {
+  return (
+    <div className={"mr-2 h-5  w-5"}>
+      <svg
+        viewBox={"0 0 24 24"}
+        className={`${active ? "fill-blue-600" : "fill-gray-400"}`}
+      >
+        <circle cx="12" cy="12" r="12" />
+        <text
+          x="50%"
+          y="50%"
+          textAnchor="middle"
+          dy=".3em"
+          className={"fill-white"}
+        >
+          {step}
+        </text>
+      </svg>
+    </div>
+  );
+};
