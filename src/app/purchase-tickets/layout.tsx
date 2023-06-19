6c8054423681781b344f4b72add68826b4c95251
devkdn <22102599+devkdn@users.noreply.github.com>
2023-06-19 14:39:26 -0500

    Add spacing to layout

--- src/app/purchase-tickets/layout.tsx
+++ src/app/purchase-tickets/layout.tsx
@@ -1,8 +1,8 @@
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div>
-      <h1>Purchase Tickets</h1>
-      <div className={"flex"}>
+    <div className={"flex flex-col items-center px-[10vw]"}>
+      <h1 className={"my-5 text-4xl font-bold"}>Purchase Tickets</h1>
+      <div className={"mb-5 flex justify-between"}>
         <Step step={1} active={true} text={"Select A Show"} />
         <Step step={2} active={false} text={"Select Ticket Quantity"} />
         <Step step={3} active={false} text={"Checkout"} />
