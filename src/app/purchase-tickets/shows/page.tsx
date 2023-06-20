6779d29686e9aef55dc36e75c6d9bc72a2357654
devkdn <22102599+devkdn@users.noreply.github.com>
2023-06-19 20:05:43 -0500

    Display formatted show

--- src/app/purchase-tickets/shows/page.tsx
+++ src/app/purchase-tickets/shows/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { shows, ShowType } from "@/data/shows";
 import { useState } from "react";
-import { getPictureUrl } from "@/utils";
+import { formatCurrency, getPictureUrl } from "@/utils";
 import Image from "next/image";
 import { useMediaQuery } from "@/utils/hooks/useMediaQuery";
 
@@ -10,24 +10,52 @@ export default function ShowSelector() {
   return (
     <div>
       {shows.map(({ id, title, date, price }) => (
-        <Show key={id} date={date} id={id} price={price} title={title} />
+        <Show
+          key={id}
+          date={date}
+          id={id}
+          price={price}
+          title={title}
+          selected={selectedShow === id}
+          handleSelect={() => setSelectedShow(id)}
+        />
       ))}
     </div>
   );
 }
 
-const Show = ({ id, title, date, price }: ShowType) => {
+type ShowProps = ShowType & {
+  selected: boolean;
+  handleSelect: () => void;
+};
+const Show = ({
+  id,
+  title,
+  date,
+  price,
+  selected,
+  handleSelect,
+}: ShowProps) => {
   const isMediumScreen = useMediaQuery("(min-width: 768px)");
   const photoWidth = 300;
   const photoHeight = isMediumScreen ? 400 : 300;
   return (
-    <div>
+    <div
+      className={"my-6 drop-shadow-sm hover:cursor-pointer"}
+      onClick={handleSelect}
+    >
       <Image
         src={getPictureUrl(id, photoWidth, photoHeight)}
         width={photoWidth}
         height={photoHeight}
         alt={title}
+        className={"rounded-t-md"}
       />
+      <div className={"flex flex-col items-center rounded-b-md border py-4"}>
+        <h2 className={"text-2xl"}>{title}</h2>
+        <p>{date}</p>
+        <p className={"font-bold"}>{formatCurrency(price)}</p>
+      </div>
     </div>
   );
 };
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,2 +1,8 @@
 export const getPictureUrl = (showId: number, width: number, height?: number) =>
   `https://picsum.photos/seed/${showId}/${width}/${!!height ? height : width}`;
+
+export const formatCurrency = (price: number) =>
+  new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+  }).format(price);
